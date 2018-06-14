2b547bdf958a2da75945bf2ba30b1d736883b612
Unknown <vennymaija.parkkila@gmail.com>
2018-06-14 13:16:45 +0300

    Initial commit

---
+++ Assets/Scripts/Nodes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Nodes : MonoBehaviour {
+
+    private Color startColor;
+    public Color hoverColor;
+    private SpriteRenderer rend;
+
+	
+	void Start ()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        startColor = rend.material.color;
+	}
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
