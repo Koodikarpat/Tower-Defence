bd827137ead0ab589717bbe771e0c7df76ad1e11
Unknown <marolli.florijan@gmail.com>
2018-06-19 12:45:21 +0300

    fix

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,7 +8,7 @@ public class Enemy : MonoBehaviour
     private Transform target;
     public GameObject targetTower;
     private int wavepointIndex = 0;
-    private int startinghealth = 100;
+    private int startinghealth = 20;
     public Image healthbar;
     private float health;
     private float distance;
@@ -72,7 +72,7 @@ public class Enemy : MonoBehaviour
         healthbar.fillAmount = health / startinghealth;
     
         if ( health <= 0) {
-            gameMaster.GetComponent<GameMaster>().goldupdate(15);
+            gameMaster.GetComponent<GameMaster>().goldupdate(30);
             Destroy(gameObject);
         }
     }
