06210f1ed24cf7f4689a76dd87838685fd16f737
OskariLamsa <40559094+OskariLamsa@users.noreply.github.com>
2018-06-28 10:33:18 +0300

    Add files via upload
    ProjectileBase?

---
+++ Assets/Scripts/ProjectileBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBase : MonoBehaviour {
+
+    protected Transform target;
+
+    public float speed = 5f;
+    public GameObject impactEffect;
+    public int damage = 0;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+    public virtual void HitTarget()
+    {
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+        target.gameObject.GetComponent<Enemy>().takeDamage(damage);
+        Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
